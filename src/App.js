ec05b886632caec2472b3241155440355ead10ce
Raj Rajhans <raj.rajhans18@vit.edu>
2019-07-28 22:42:46 +0530

    Nav Component Started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Nav from './Nav'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props){
+        super(props)
+    }
+
+    renderContent(){
+        return(
+            <span>Empty</span>
+        )
+    }
+
+    render() {
+        return(
+            <div className={"App"}>
+                <Nav />
+                <main className={"App-content"}>
+                    {this.renderContent()}
+                </main>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () =>(
+  <nav className={"App-nav"}>
+      <ul>
+          <li className={"App-nav-item"}><a>Items</a></li>
+          <li className={"App-nav-item"}><a>Cart</a></li>
+      </ul>
+  </nav>
+);
+
+export default Nav;
