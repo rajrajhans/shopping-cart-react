7f3c626b5e969a1afca30c23d22af9b254d6450f
Raj Rajhans <raj.rajhans18@vit.edu>
2019-07-30 17:56:17 +0530

    Count of how many times an item is added in cart

--- src/CartPage.js
+++ src/CartPage.js
@@ -2,15 +2,39 @@ import React from 'react'
 import ItemPage from './ItemPage'
 
 class CartPage extends ItemPage{
-    // eslint-disable-next-line no-useless-constructor
     constructor(props){
-        super(props)
+        super(props);
+
+        this.countItems = this.countItems.bind(this);
+    }
+
+    countItems(cart){
+        let cartIds = [];
+        let netCount = 0;
+        let cartIdCount = {};
+        for (let i = 0; i < cart.length; i++){
+            if (cart[i].id in cartIds){
+                cartIdCount[cart[i].id] += 1;
+            }
+            else{
+                netCount += 1;
+                cartIds.push(cart[i].id);
+                cartIdCount[cart[i].id] = 1;
+            }
+        }
+        console.log("cartIdCount", cartIdCount);
+        console.log("cartIds", cartIds);
+
+        return cartIdCount;
     }
 
     render(){
             return(
-                <ItemPage items={ this.props.CartItems} />
-            )
+                //<ItemPage items={this.props.CartItems} />
+                <div>
+                    <p>{this.countItems(this.props.CartItems)['1']}</p>
+                </div>
+                )
     }
 }
 
--- src/data.js
+++ src/data.js
@@ -1,34 +1,34 @@
 let testItems = [
     {
-        "id" : 1,
+        "id" : 0,
         "name": "Learning React: Functional Web Development with React and Redux ",
         "image" : "https://images-eu.ssl-images-amazon.com/images/I/51Q43WRXJzL._SX240_.jpg",
         "price" : "878.75",
         "description" : "Authors Alex Banks and Eve Porcello show you how to create UIs with this small JavaScript library that can deftly display data changes on large-scale, data-driven websites without page reloads"
     },
     {
-        "id" : 2,
+        "id" : 1,
         "name": "Pears Soft and Fresh Shower Gel, 250ml ",
         "image" : "https://images-na.ssl-images-amazon.com/images/I/51CiQj71YzL._SX240_.jpg",
         "price" : "145.00",
         "description" : "Made with 98 percent pure glycerin, Dermatologically tested to be mild on skin"
     },
     {
-        "id" : 3,
+        "id" : 2,
         "name": "Classmate Pulse 6 Subject Notebook ",
         "image" : "https://images-na.ssl-images-amazon.com/images/I/51LTAUNKg9L._AC_SX240_.jpg",
         "price" : "120.00",
         "description" : "6 Subject Notebook-Unruled, 300 Pages, Spiral Binding, Soft Cover, 240mm*180mm "
     },
     {
-        "id" : 4,
+        "id" : 3,
         "name": "Mesh Desk Organizer, Black ",
         "image" : "https://images-na.ssl-images-amazon.com/images/I/81NpfftEBDL._SX240_.jpg",
         "price" : "449.00",
         "description" : "Desk organizer - made of black, metal wire mesh. Divided compartments keep pens, pencils, markers, and more organized "
     },
     {
-        "id" : 5,
+        "id" : 4,
         "name": "Upon an Old Wall Dreaming by Ruskin Bond",
         "image" : "https://images-eu.ssl-images-amazon.com/images/I/51Wii3dYmSL.jpg",
         "price" : "252.70",
